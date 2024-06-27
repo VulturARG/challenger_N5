a4e00f412fa8136f551a9e9c44a8a18b2fae3ec6
VulturARG <luisbriones@gmail.com>
2024-06-27 15:08:49 -0300

    Fix serializer.

--- api/infrastructure/django/serializers/infraction_serializer.py
+++ api/infrastructure/django/serializers/infraction_serializer.py
@@ -6,4 +6,8 @@ from api.infrastructure.django.models.infraction import Infraction
 class InfractionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Infraction
-        fields = ["vehicle", "timestamp", "comments"]
+        fields = ["timestamp", "placa_patente", "comentarios"]
+
+    placa_patente = serializers.CharField(source="vehicle.license_plate")
+    timestamp = serializers.DateTimeField()
+    comentarios = serializers.CharField()

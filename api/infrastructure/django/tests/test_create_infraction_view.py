0740f5bb504ca2132a6ce3dce2598484d2ce9513
VulturARG <luisbriones@gmail.com>
2024-06-28 15:42:01 -0300

    Add test and refactor.

--- api/infrastructure/django/tests/test_create_infraction_view.py
+++ api/infrastructure/django/tests/test_create_infraction_view.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
+from django.utils.timezone import make_aware
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from api.domain.entities.infraction_entity import InfractionEntity
+from api.infrastructure.django.models.infraction import Infraction
 from api.infrastructure.django.tests.test_set_up import TestSetUp
 
 
@@ -13,7 +18,7 @@ class TestCreateInfractionView(TestSetUp):
         self._data = {
             "placa_patente": "ABC123",
             "timestamp": "2024-07-01 12:35:55",
-            "comentarios": "hjsklhjfslkjf",
+            "comentarios": "comments",
         }
         refresh = RefreshToken.for_user(self.user)
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
@@ -27,14 +32,31 @@ class TestCreateInfractionView(TestSetUp):
     def test_create_infraction_success(self):
         expected = {
             "message": {
-                "comentarios": "hjsklhjfslkjf",
+                "comentarios": "comments",
                 "placa_patente": "ABC123",
                 "timestamp": "2024-07-01 12:35:55",
             }
         }
+
         response = self.client.post(self._url_path, data=self._data, format="json")
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(expected, response.data)
+
+    def test_create_infraction_success_data_in_db(self):
+        datetime_naive = datetime(2024, 7, 1, 12, 35, 55)
+        datetime_aware = make_aware(datetime_naive)
+        expected = InfractionEntity(
+            vehicle=self._vehicle_1.to_entity(),
+            timestamp=datetime_aware,
+            comments="comments",
+        )
+
+        response = self.client.post(self._url_path, data=self._data, format="json")
+        infraction = Infraction.objects.get(
+            vehicle__license_plate=expected.vehicle.license_plate
+        )
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(expected, infraction.to_entity())
 
     def test_create_infraction_missing_fields(self):
         expected = {
@@ -51,7 +73,7 @@ class TestCreateInfractionView(TestSetUp):
         data = {
             "placa_patente": "NotExist",
             "timestamp": "2024-07-01 12:35:55",
-            "comentarios": "hjsklhjfslkjf",
+            "comentarios": "comments",
         }
         response = self.client.post(self._url_path, data=data, format="json")
         self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
